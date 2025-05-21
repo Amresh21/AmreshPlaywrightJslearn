75898e05922ab47b7c7b7b0150ef25d780d46dac
Amresh21 <amresh21293@gmail.com>
2025-05-21 06:56:15 +0530

    Testcase for locator updated

--- tests/playwrightUIAutomation/TC01LocatorDemo.spec.js
+++ tests/playwrightUIAutomation/TC01LocatorDemo.spec.js
@@ -5,25 +5,25 @@ import { test, expect } from '@playwright/test';
 //PAGE
 //https://www.saucedemo.com/
 test('Validate the secret sauce demo website', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
+  
+  await page.goto('/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="username"]').press('Tab');
-  await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
   await page.locator('[data-test="shopping-cart-link"]').click();
   await page.locator('[data-test="checkout"]').click();
   await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('Test');
-  await page.locator('[data-test="lastName"]').click();
-  await page.locator('[data-test="lastName"]').fill('Testlastname');
-  await page.locator('[data-test="postalCode"]').click();
-  await page.locator('[data-test="postalCode"]').fill('93282');
+  await page.locator('[data-test="firstName"]').fill('Amresh');
+  await page.locator('[data-test="firstName"]').press('Tab');
+  await page.locator('[data-test="lastName"]').fill('Chakkarapani');
+  await page.locator('[data-test="lastName"]').press('Tab');
+  await page.locator('[data-test="postalCode"]').fill('600100');
   await page.locator('[data-test="continue"]').click();
   await page.locator('[data-test="finish"]').click();
-  
+
 });
 
 
@@ -37,3 +37,16 @@ test('Validate the secret sauce demo website with Invalid username', async ({ pa
     await page.locator('[data-test="login-button"]').click();
 
   });
+
+test('Checking with Invalid Credentials', async ({ page }) => {
+  await page.goto('https://saucelabs.com/');
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  const page1 = await page1Promise;
+  await page1.goto('https://accounts.saucelabs.com/am/XUI/#login/');
+  await page1.getByRole('textbox', { name: 'User Name' }).fill('dsfsdf');
+  await page1.getByRole('textbox', { name: 'User Name' }).press('Tab');
+  await page1.getByRole('textbox', { name: 'Password' }).fill('sdffsdf');
+  await page1.getByRole('button', { name: 'Log in' }).click();
+  await page1.getByText('User name/password').click();
+});
